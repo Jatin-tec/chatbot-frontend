5223d769a9be13ed96fbeb72bb7f261804b30e97
Harshal Rathore <harshalrathore2014@gmail.com>
2022-08-26 01:51:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,36 +5,59 @@ import Chats from "./Chats";
 import "./App.css";
 function App() {
   const [inputText, setInputText]=useState("");
+  const [botInputText, setBotInputText]=useState("");
+  const [items, setItems]=useState([]);
+  const [botItems, setBotItems]=useState([]);
   function handlechange(event){
     const Task= event.target.value;
     setInputText(Task);
   }
-  const [items, setItems]=useState([]);
+  function handleBotChange(event){
+    const Task= event.target.value;
+    setBotInputText(Task);
+  }
   function handleKeyPress(event){
-    if(event.key === 'Enter' && document.getElementById("inputField").value.length !== 0){
+    if(event.key === 'Enter' ){
       setItems(prevItems => {
         return [inputText,...prevItems];
       });
       setInputText(""); }
+  }function bothandleKeyPress(event){
+    if(event.key === 'Enter' && document.getElementById("botinputField").value.length !== 0){
+      setBotItems(prevItems => {
+        return [inputText,...prevItems];
+      });
+      setBotInputText(""); }
   }
-  function Add(){
 
-    if(document.getElementById("inputField").value.length !== 0){setItems(prevItems => {
+  function Add(){
+    if(document.getElementById("inputField").value.length !== 0){
+      setItems(prevItems => {
       return [inputText,...prevItems];
     });
-    setInputText("");}
+    setInputText("");
+
+  }
   }
   return (
     <div className="Main-Cointainer">
    <div className="Content">
-    <div className="Main-Cointent">
+    <div className="Main-Cointent" >
 
     {items.map(chat => (
             <Chats
             text={chat}
+            color="white"
+            wordColor="black"
             />
           ))}
-
+    {botItems.map(chat =>(
+      <Chats
+      text={chat}
+      color="green"
+      wordColor="black"
+      />
+    ))}
      
     </div>
    
@@ -43,6 +66,12 @@ function App() {
    
    <div className="InputChatFeild">
     <input id='inputField' 
+        value={inputText}
+        type="text"
+        onChange={handlechange}
+        onKeyPress={handleKeyPress}
+         />
+         <input id='botinputField' 
         value={inputText}
         type="text"
         onChange={handlechange}
