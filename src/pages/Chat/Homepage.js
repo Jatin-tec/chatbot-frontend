b643c5338c9b0c9660406f222892d4c2986cf59f
Praahant <76206077+Praahant@users.noreply.github.com>
2022-08-26 11:09:05 +0530

    commit

--- src/pages/Chat/Homepage.js
+++ src/pages/Chat/Homepage.js
@@ -17,13 +17,24 @@ export default function Homepage() {
             <div className='Heading'>Covid-19</div>
 
           </div>
-          <div className="new">Chatbot to detect the Covid-19 based on the patient symptoms.</div>
+          <div className="new">Chatbot to detect the Covid-19 based on the symptoms of patient.</div>
           <div className='filler'></div>
           <div className='TestButton'>
             <Button variant="contained">Test</Button>
           </div></div>
+        <div className='First' >
+          <div className='FirstContent'>
+            <div className='Heading'>Appointment Booking</div>
+
+          </div>
+          <div className="new">Book Appointment with your preffered doctor or recommended one</div>
+          <div className='filler'></div>
+          <div className='TestButton'>
+            <Button variant="contained" >Test</Button>
+
+          </div></div>
 
       </div>
-    </div>
+    </div >
   )
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,11 @@
 import Homepage from "../pages/Chat/Homepage";
 import ChatUI from "../pages/Home/chatUI";
 import SignIn from "../pages/Login/Login";
+import Basics from "../components/Chat/Basic";
 // import MainChat from "../pages/ChatUserInterface/MainChat";
 const allRoutes = [
     { 'path': '/', component: Homepage },
-    { 'path': '/chat', component: ChatUI },
+    { 'path': '/chat', component: Basics },
     { 'path': '/Login', component: SignIn },
     // { 'path': '/Main', component: MainChat },
 ]
